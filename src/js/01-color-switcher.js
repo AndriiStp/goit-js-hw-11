83ad4f3277ea41e1d469d0f3680d115601eefe1a
Andrii Stepanenko <andrii.stp@gmail.com>
2023-04-23 23:38:49 +0300

    full

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+start.addEventListener('click', () => {
+  changer = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (changer) {
+    start.setAttribute('disabled', 'disabled');
+  }
+});
+
+stop.addEventListener('click', () => {
+  clearInterval(changer);
+  start.removeAttribute('disabled', 'disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
---
+++ src/js/03-promises.js
@@ -0,0 +1,41 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector(`.form`);
+
+form.addEventListener(`submit`, onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+  form.reset();
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
